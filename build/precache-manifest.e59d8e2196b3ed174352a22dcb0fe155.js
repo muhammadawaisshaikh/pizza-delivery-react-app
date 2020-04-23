9a730ff2aea66cb26efc11a79319f4751d4d7ab0
awaisshaikh94@gmail.com <awaisshaikh94@gmail.com>
2020-04-23 20:31:20 +0500

    npm run-script build

---
+++ build/precache-manifest.e59d8e2196b3ed174352a22dcb0fe155.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4193ca9ab6b319087c2c763b2bd16332",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c5252aee6d5b09127ff",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b2a59da62cbad25de706",
+    "url": "/static/css/main.58232387.chunk.css"
+  },
+  {
+    "revision": "2c5252aee6d5b09127ff",
+    "url": "/static/js/2.023029e6.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/static/js/2.023029e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2a59da62cbad25de706",
+    "url": "/static/js/main.c93bf9f7.chunk.js"
+  },
+  {
+    "revision": "cfcc20dcb72a8a00bc54",
+    "url": "/static/js/runtime-main.348d6efa.js"
+  },
+  {
+    "revision": "7b91cfcfa9d29eb68eceda6813ae4416",
+    "url": "/static/media/Pizza_img.7b91cfcf.jpg"
+  },
+  {
+    "revision": "a1c78ff3de152c4a01f13d42567ea19d",
+    "url": "/static/media/menu-list-banner.a1c78ff3.jpg"
+  },
+  {
+    "revision": "f4c0be4c1c6c7f0b6a70ab8dedc42358",
+    "url": "/static/media/no-data.f4c0be4c.png"
+  }
+]);
